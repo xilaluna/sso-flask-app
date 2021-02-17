6762658d9ae5f28a3787486d2acd9186cd9f2d6f
Xila Luna <xilaluna2@gmail.com>
2021-02-16 21:54:27 -0800

    init commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return """
+	<a href="/login">Login</a>
+	"""
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
